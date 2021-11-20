f1eaad3db70f4f4e35d3acd7a32eb1b8dcbbf5d1
DanielEgidiio <danielegidioti@gmail.com>
2021-11-20 20:28:07 -0300

    Criação da aplicação e do seu Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <>
+            <div>
+                <span className="header" >🦸 Heros Battle 🦸</span>
+            </div>
+        </>
+    )
+}
+
+export default Header;
